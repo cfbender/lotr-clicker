098a70dffc3e74fbfdaf436b6cf0d31f4b4a80df
Cody Bender <codyfbender@gmail.com>
2019-11-16 10:22:05 -0700

    :snowflake:

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,27 +1,12 @@
-import React from 'react'
-import Link from 'next/link'
-
-const links = [
-  { href: 'https://zeit.co/now', label: 'ZEIT' },
-  { href: 'https://github.com/zeit/next.js', label: 'GitHub' }
-].map(link => {
-  link.key = `nav-link-${link.href}-${link.label}`
-  return link
-})
+import React from "react";
+// import Link from 'next/link'
 
 const Nav = () => (
   <nav>
     <ul>
       <li>
-        <Link href='/'>
-          <a>Home</a>
-        </Link>
+        <a>Home</a>
       </li>
-      {links.map(({ key, href, label }) => (
-        <li key={key}>
-          <a href={href}>{label}</a>
-        </li>
-      ))}
     </ul>
 
     <style jsx>{`
@@ -51,6 +36,6 @@ const Nav = () => (
       }
     `}</style>
   </nav>
-)
+);
 
-export default Nav
+export default Nav;
